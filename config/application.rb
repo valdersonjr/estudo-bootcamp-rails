a1b914b9d7711642fc6b061705446b618a6481a4
valdersonjr <valdersonpontes@gmail.com>
2023-09-28 14:35:12 -0300

    2.1, 2.2 - validação customizada

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module EcommerceApi
     config.load_defaults 6.0
     config.i18n.default_locale = :'pt-BR'
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}')]
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -35,5 +35,6 @@ module EcommerceApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths += %W["#{config.root}/app/validators/"]
   end
 end
