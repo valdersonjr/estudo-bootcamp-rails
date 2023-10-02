76150b17359ce5fd8e486b7e07ded20eb20235f7
valdersonjr <valdersonpontes@gmail.com>
2023-10-02 13:19:52 -0300

    2.15 - busca por nome

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  include NameSearchable
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   has_many :product_categories, dependent: :destroy
   has_many :products, through: :product_categories
