5520bca2d08e54dff3a7810686c15028afd7d410
valdersonjr <valdersonpontes@gmail.com>
2023-11-06 07:35:25 -0300

    product view change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://localhost:3001'
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['acess-token', 'client', 'expiry', 'token-type', 'uid']
+      expose: ['access-token', 'client', 'expiry', 'token-type', 'uid'],
+      credentials: true
   end
 end
