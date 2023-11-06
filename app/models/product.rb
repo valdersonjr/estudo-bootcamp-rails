050e01a62c33a37d60beba4aeb5c68c7922fe261
valdersonjr <valdersonpontes@gmail.com>
2023-11-06 13:15:07 -0300

    Sf 1.2 - Serviço de dados da home

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,7 +7,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validates :featured, presence: true
+  validates :featured, presence: true, inclusion: { in: [true, false] }
   has_many :product_categories, dependent: :destroy
   has_many :categories, through: :product_categories
 
