3e93a0ea3a9081ec8aa667c964cb8c504e243ed7
valdersonjr <valdersonpontes@gmail.com>
2023-11-08 09:19:37 -0300

    sf 1.7 - Detalhes do produto

--- app/controllers/storefront/v1/products_controller.rb
+++ app/controllers/storefront/v1/products_controller.rb
@@ -1,15 +1,20 @@
 module Storefront::V1
     class ProductsController < ApplicationController
   
-    def index
-        @service = Storefront::ProductsFilterService.new(search_params)
-        @service.call
-    end
+        def index
+            @service = Storefront::ProductsFilterService.new(search_params)
+            @service.call
+        end
+        
+        def show
+            @product = Product.find(params[:id])
+        end
 
-    private
+        private
 
-    def search_params
-        params.permit(:search, :productable, :page, :length, order: {}, category_ids: [], 
-                      price: [:min, :max], release_date: [:min, :max]).merge(productable: :game)
+        def search_params
+            params.permit(:search, :productable, :page, :length, order: {}, category_ids: [], 
+                        price: [:min, :max], release_date: [:min, :max]).merge(productable: :game)
+        end
     end
 end
